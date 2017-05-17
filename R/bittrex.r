e49eab50c10eae9169992915e616ed067f50bb3c
Michael Kane <kaneplusplus@gmail.com>
2017-05-16 23:29:54 -0400

    Updated tests and documentation.

--- R/bittrex.r
+++ R/bittrex.r
@@ -1,3 +1,63 @@
+#' @title Client for the Bittrex Crypto-Currency Exchange
+#' @name bittrex-package
+#' @aliases bittrex-package bittrex
+#' @docType package
+#' @references \url{https://bittrex.com/}
+#' \url{https://github.com/kaneplusplus/bittrex}
+#' @description
+#' This software is in no way affiliated, endorsed, or approved by
+#' the Bittrex crypto-currency exchange or any of its affiliates. It comes with
+#' absolutely no warranty and should not be used in actual trading
+#' unless the user can read and understand the source and know what you are
+#' doing.
+#' 
+#' Package 'bittrex' is an R implementation of the REST interface used 
+#' by the Bittrex crypto-currency exchange \url{https://bittrex.com/}. It 
+#' provides functions for all endpoints currently (as of May 16, 2017) 
+#' supported by the exchange. This includes the ability 
+#' to retrieve price, volume, and orderbook information as well as the ability
+#' to trade crypto-currencies.
+#' 
+#' Calls to the exchange are categorized as either public, which includes 
+#' requests for price, volume, and order book information, and private, which 
+#' includes all requests requiring an account including placing buy or sell 
+#' orders. Public calls can be used directly by installing the package. 
+#' Private calls require creating an account at 
+#' \url{https://https://bittrex.com/account/Register} and creating an API and 
+# secret key with appropriate permissions.
+#' 
+#' Private calls retrieve the API and secret key using the BITTREX_API_KEY and 
+#' BITTREX_SECRET_KEY environment variables. These may be set by the user 
+#' before opening the R session or, they can be set using the 
+#' 'bittrex_authenticate' function.
+#' 
+#' Public Function Calls
+#' \itemize{
+#' \item{getcurrencies: }{all supported currencies at Bittrex along with other meta data}
+#' \item{getmarkethistory: }{the latest trades that have occured for a specified market}
+#' \item{getmarkets: }{the open and available trading markets at Bittrex along with other meta data}
+#' \item{getmarketsummaries: }{the last 24 hour summary of all active exchanges}
+#' \item{getmarketsummary: }{the last 24 hour summary of all active exchanges}
+#' \item{getorderbook: }{the orderbook for a given market}
+#' \item{getticker: }{the current tick values for a market}
+#' }
+#' Private Function Calls
+#' \itemize{
+#' \item{bittrex_authenticate: }{provide user authentication data}
+#' \item{buylimit: }{place a buy limit order}
+#' \item{cancel: }{cancel buy or sell order}
+#' \item{getbalances: }{account balances for currencies}
+#' \item{getbalance: }{account balance for a specified currency}
+#' \item{getdepositaddress: }{retrieve or generate an address for a specific currency}
+#' \item{getdeposithistory: }{retrieve your deposit historye
+#' \item{getopenorders: }{order data for all open orders}
+#' \item{getorder: }{retrieve a single order by uuid}
+#' \item{getorderhistory: }{recent order history for an account }
+#' \item{getwithdrawlhistory: }{retrieve your withdrawal history}
+#' \item{selllimit: }{place a sell limit order}
+#' \item{withdraw: }{withdraw funds from your account}
+#' }
+NULL
 
 camel_to_snake = function(x) {
   gsub("^_+", "", tolower(gsub("([A-Z])", "_\\1", x)))
@@ -25,19 +85,10 @@ timestamp_to_posix = function(x) {
 }
 
 public_url = "https://bittrex.com/api/v1.1/public"
-
 account_url = "https://bittrex.com/api/v1.1/account"
-
 market_url = "https://bittrex.com/api/v1.1/market"
 
-#' @importFrom httr GET content add_headers
-#' @importFrom openssl sha512
-priv_req = function(req) {
-  str_time = as.character(as.integer(Sys.time()))
-  req = paste0(req, "&nonce=", str_time)
-  sig = sha512(req, Sys.getenv("BITTREX_SECRET_KEY"))
-  content(GET(req, add_headers(apisign=sig)), type="application/json")
-}
+# PUBLIC API
 
 #' @importFrom httr GET content
 #' @export
@@ -119,16 +170,28 @@ getorderbook = function(market, type=c("both", "buy", "sell"), depth=50) {
 
 #' @importFrom httr GET content
 #' @export
-getmarkethistory = function(market, count=20) {
+getmarkethistory = function(market) {
   resp = content(GET(paste(public_url, 
-    paste0("getmarkethistory?market=", market, "&count=", count), sep="/")),
+    paste0("getmarkethistory?market=", market), sep="/")),
     type="application/json")
   if (resp$succes) {
     resp$result = result_to_df(resp$result)
-    resp$result$time_stamp = timstamp_to_posix(resp$result$time_stamp)
+    resp$result$time_stamp = timestamp_to_posix(resp$result$time_stamp)
   }
   resp
 }
+
+# PRIVATE API
+
+#' @importFrom httr GET content add_headers
+#' @importFrom openssl sha512
+priv_req = function(req) {
+  str_time = as.character(as.integer(Sys.time()))
+  req = paste0(req, "&nonce=", str_time)
+  sig = sha512(req, Sys.getenv("BITTREX_SECRET_KEY"))
+  content(GET(req, add_headers(apisign=sig)), type="application/json")
+}
+
 
 #' @export
 bittrex_authenticate = function(api_key, secret_key) {
--- tests/testthat/test-public-calls.R
+++ tests/testthat/test-public-calls.R
@@ -1,25 +1,6 @@
-context('C-Cex Public Calls')
+library(testthat)
 
-test_that('the "coinnames" function works.', {
-  resp = coinnames()
-  expect_true(resp$success)
-  expect_is(resp$result, 'data.frame')
-  Sys.sleep(2)
-})
-
-test_that('The "getbalancedistribution" function works.', {
-  resp = getbalancedistribution("trump")
-  expect_true(resp$success)
-  expect_is(resp$result, 'numeric')
-  Sys.sleep(2)
-})
-
-test_that('The "getmarkethistory" function works.', {
-  resp = getmarkethistory("usd-btc")
-  expect_true(resp$success)
-  expect_is(resp$result, 'data.frame')
-  Sys.sleep(2)
-})
+context('Bittrex Public Calls')
 
 test_that('The "getmarkets" function works.', {
   resp = getmarkets()
@@ -35,31 +16,46 @@ test_that('The "getmarketsummaries" function works.', {
   Sys.sleep(2)
 })
 
-test_that('The "getorderbook" function works.', {
-  resp = getorderbook("usd-btc")
-  expect_true(resp$success)
-  expect_is(resp$result, 'list')
-  expect_is(resp$result[[1]], 'data.frame')
-  expect_is(resp$result[[2]], 'data.frame')
-  Sys.sleep(2)
-})
-
-test_that('The "pairs" function works.', {
-  resp = pairs()
-  expect_true(resp$success)
-  expect_is(resp$result, 'character')
-  Sys.sleep(2)
-})
-
-test_that('The "prices" function works.',{
-  resp = prices()
+test_that('The "getcurrencies" function works.', {
+  resp = getcurrencies()
   expect_true(resp$success)
   expect_is(resp$result, 'data.frame')
   Sys.sleep(2)
 })
 
-test_that('The "volume" function works.', {
-  resp = volume("btc")
+test_that('The "getticker" function works.', {
+  resp = getticker("btc-ltc")
+  expect_true(resp$success)
+  expect_is(resp$result, 'data.frame')
+  Sys.sleep(2)
+})
+
+test_that('The "getmarketsummaries" function works.', {
+  resp = getmarketsummaries()
+  expect_true(resp$success)
+  expect_is(resp$result, 'data.frame')
+  Sys.sleep(2)
+})
+
+test_that('The "getmarketsummary" function works.', {
+  resp = getmarketsummary("btc-ltc")
+  expect_true(resp$success)
+  expect_is(resp$result, 'data.frame')
+  Sys.sleep(2)
+})
+
+test_that('The "getorderbook" function works.', {
+  resp = getorderbook("btc-ltc")
+  expect_true(resp$success)
+  expect_is(resp$result, 'list')
+  expect_equal(names(resp$result), c("buy", "sell"))
+  expect_is(resp$result$buy, 'data.frame')
+  expect_is(resp$result$sell, 'data.frame')
+  Sys.sleep(2)
+})
+
+test_that('The "getmarkethistory" function works.', {
+  resp = getmarkethistory("btc-ltc")
   expect_true(resp$success)
   expect_is(resp$result, 'data.frame')
   Sys.sleep(2)
