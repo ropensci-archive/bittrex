d491d303131c5a72acd28dc5556b27eef976b70d
Michael Kane <kaneplusplus@gmail.com>
2017-05-19 17:28:47 -0400

    Updates.

--- R/bittrex.r
+++ R/bittrex.r
@@ -101,7 +101,7 @@ market_url = "https://bittrex.com/api/v1.1/market"
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A \code{data.frame} with the market currencies, 
 #'    base currencies, base currency long name, minimum trade size, maket name,
 #'    if the market is active, when the market was created, market notices,
@@ -130,7 +130,7 @@ getmarkets = function() {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A \code{data.frame} with the currency ticker, 
 #'    currency, a minimum confirmation number, the transaction fee, if the
 #'    currency is active, the coin type, the base address, and currency 
@@ -159,7 +159,7 @@ getcurrencies = function() {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A \code{data.frame} with the bid, ask, and last 
 #'                   trasaction price.}
 #' }
@@ -187,7 +187,7 @@ getticker = function(market) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A \code{data.frame} with one row per market and, for
 #'                   each market: the market name, the high, the low, the
 #'                   volume, the last trade, the last trade price, the 
@@ -225,7 +225,7 @@ getmarketsummaries = function() {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A \code{data.frame} with one row and columns corresponding
 #'                   to: the market name, the high, the low, the
 #'                   volume, the last trade, the last trade price, the 
@@ -267,7 +267,7 @@ getmarketsummary = function(market) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A named list with the buy and sell orders (depending
 #'    on the specified \code{type} parameter. If \code{type} is "buy" or
 #'    "both" then the list will contain a element named "buy" with
@@ -313,7 +313,7 @@ getorderbook = function(market, type=c("both", "buy", "sell"), depth=50) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{A code{data.frame} containing recent trade information
 #'    including the order type, time, quantity, price, and fill type.}
 #' }
@@ -380,9 +380,9 @@ bittrex_authenticate = function(api_key, secret_key) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
-#'  \item{result:  }{a named list, called "uuid" whose element is an integer
-#'    identifying the order. This value is used to query the status of
+#'                   successful, otherwise and empty string.}
+#'  \item{result:  }{a named list, with element "uuid" whose element is an 
+#'    integer identifying the order. This value is used to query the status of
 #'    of the order with either the \code{link{getorder}} or 
 #'    \code{link{getopenorders}} function. When the order is fulfilled it
 #'    appears in the order history \code{data.frame} returned by the
@@ -438,7 +438,7 @@ buy = function(market, quantity, rate, type=c("limit", "market")) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{a named list, called "uuid" whose element is an integer
 #'    identifying the order. This value is used to query the status of
 #'    of the order with either the \code{link{getorder}} or 
@@ -489,7 +489,7 @@ sell = function(market, quantity, rate, type=c("market", "limit")) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{always NULL}
 #' }
 #' @examples
@@ -518,7 +518,7 @@ cancel = function(uuid) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{a \code{data.frame} providing information about the 
 #'    open orders including (but not limited to) the market, quantity remaining
 #'    in the order, the type of order, and when the order was opened.
@@ -539,7 +539,6 @@ getopenorders = function(market) {
              "&market=", market), sep="/")
   }
   ret = priv_req(req)
-  # TODO: check that this right
   if ( (ret$success == TRUE) && (length(ret$result) > 0)) {
     ret$result = result_to_df(ret$result)
   }
@@ -557,7 +556,7 @@ getopenorders = function(market) {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{a \code{data.frame} with the currencies, balances, 
 #'    available funds, the amount of any pending transactions, and 
 #'    crypographic addresses that can be used to receive funding.
@@ -593,7 +592,7 @@ getbalances = function() {
 #' \itemize{
 #'  \item{success: }{a boolean indicating if the request successful?}
 #'  \item{message: }{a string describing the error if the request was not 
-#'                   successful, otherwise and empty string."}
+#'                   successful, otherwise and empty string.}
 #'  \item{result:  }{a \code{data.frame} with the currency, balance, 
 #'    available funds, the amount of any pending transactions, and 
 #'    crypographic addresses that can be used to receive funding.
@@ -613,5 +612,154 @@ getbalances = function(currency) {
     ret$result = as_data_frame(ret$result)
   }
   ret
+}
+
+#' @title Retrieve the Address for a Specified Currency
+#' @description The \code{getdepositaddress} retrieves or creates the account 
+#' deposit address for a specified currency.
+#' @references \url{https://bittrex.com/api/v1.1/account/getdepositaddress?apikey=API_KEY&currency=VTC}
+#' @param currency currency to get the deposit address
+#' @return A named list with the following elements:
+#' \itemize{
+#'  \item{success: }{a boolean indicating if the request successful?}
+#'  \item{message: }{a string describing the error if the request was not 
+#'                   successful, otherwise and empty string. If an 
+#'                   address has not been generated this field will have value
+#'                   "ADDRESS_GENERATING" until it is available.}
+#'  \item{result:  }{a \code{data.frame} with the one row and columns 
+#'                   providing the currency and the address.}
+#' }
+#' @examples
+#' \dontrun{
+#' # Get the address of the bitcoin currency.
+#' getdepositaddress("btc")
+#' }
+#' @export
+getdepositaddress = function(currency) {
+  req = paste(account_url,
+    paste0("getdepositaddress?apikey=", Sys.getenv("BITTREX_API_KEY"),
+      "&currency=", currency), sep="/")
+  ret = priv_req(req)
+  if (ret$success) {
+    ret$result = as_data_frame(ret$result)    
+  }
+  ret
+}
+
+#' @references \url{https://bittrex.com/api/v1.1/account/withdraw?apikey=API_KEY&currency=EAC&quantity=20.40&address=EAC_ADDRESS}
+#' @param currency the currency to withdraw.
+#' @param quantity the quantity of the currency to withdraw.
+#' @param address where to send the funds.
+#' @param paymentid CryptoNotes/BitShareX/Nxt optional field (memo/paymentid).
+#' @return A named list with the following elements:
+#' \itemize{
+#'  \item{success: }{a boolean indicating if the request successful?}
+#'  \item{message: }{a string describing the error if the request was not 
+#'                   successful, otherwise and empty string.}
+#'  \item{result:  }{a named list, with element "uuid" whose element is an 
+#'    integer identifying the order.}
+#'  }
+#' @examples
+#' \dontrun{
+#' # Send the author your bitcoins.
+#' widthdraw("btc", 10, "1Q6WissSMNF7NCNw3sDXQ2F7AbrSCYouj2")
+#' }
+#' @export
+widthdraw = function(currency, quantity, address, paymentid) {
+  req = paste(account_url, 
+    paste0("withdraw?apikey=", Sys.getenv("BITTREX_API_KEY"),
+      "&currency=", currency, "&quantity=", quantity, "&address=", address),
+      sep="/")
+  if (!missing(paymentid))
+    req = paste0(req, "&paymentid=", paymentid)
+  priv_req(req)
+}
+
+#' @title Order Data for a Specified Order
+#' @description The \code{getorder} function retrieves open order data 
+#' on the Bittrex crypto-currency 
+#' exchange \url{https://bittrex}. This function
+#' can be used after you provide information for authentication.
+#' @seealso \code{\link{bittrex_authenticate}} \code{\link{getopenorders}}.
+#' @references \url{https://bittrex.com/api/v1.1/account/getorder&apikey=API_KEY&uuid=0cb4c4e4-bdc7-4e13-8c13-430e587d2cc1}
+#' @param uuid the uuid of the order.
+#' @return A named list with the following elements:
+#' \itemize{
+#'  \item{success: }{a boolean indicating if the request successful?}
+#'  \item{message: }{a string describing the error if the request was not 
+#'                   successful, otherwise and empty string."}
+#'  \item{result:  }{a \code{data.frame} providing information about the 
+#'    open order including (but not limited to) the market, quantity remaining
+#'    in the order, the type of order, and when the order was opened.
+#'  }
+#' }
+#' @examples
+#' \dontrun{
+#' getorder(uuid)
+#' }
+#' @export
+getorder = function(uuid) {
+  req = paste(account_url, paste0("getorder?apikey=", 
+    Sys.getenv("BITTREX_API_KEY"), "&uuid=", uuid), sep="/")
+  p = priv_req(req)
+  ret = NULL
+  if (length(resp$result) > 0) {
+    for(i in 1:length(resp$result)) {
+      for (j in 1:length(resp$result[[i]])) {
+        if (is.null(resp$result[[i]][[j]])) resp$result[[i]][[j]] = NA
+      }
+    }
+    ret = Reduce(rbind, Map(as_data_frame, resp$result))
+    names(ret) = camel_to_snake(names(ret))
+  }
+  resp$result = ret
+  resp
+}
+
+#' @title Recent Order History for an Account
+#' @description The \code{getorderhistory} function retrieves order history
+#' data on the Bittrex crypto-currency exchange \url{https://bittrex.com}. This 
+#' function can be used after you provide information for authentication.
+#' @seealso \code{\link{bittrex_authenticate}}
+#' @references \url{https://bittrex.com/api/v1.1/account/getorderhistory}
+#' @param market (optional) the market on which you would like to see all 
+#' open orders. If not specified, then completed orders for all markets are 
+#' returned.
+#' @return A named list with the following elements:
+#' \itemize{
+#'  \item{success: }{a boolean indicating if the request successful?}
+#'  \item{message: }{a string describing the error if the request was not 
+#'                   successful, otherwise and empty string."}
+#'  \item{result:  }{a \code{data.frame} providing data about 
+#'    previously completed orders including the order uuid, the exchange
+#'    the time of the order, the order type, the limit, the quantity, the
+#'    quantity remaining, the commission, the price, the price per unit,
+#'    and whether or not it was a conditional trade.
+#'  }
+#' }
+#' @examples
+#' \dontrun{
+#' getorderhistory()
+#' }
+#' @export
+getorderhistory = function(market) {
+  req = paste(account_url, paste0("getorderhistory?apikey=",
+    Sys.getenv("BITTREX_API_KEY")), sep="/")
+  if (!missing(market)) 
+    req = paste0(req, "&markte=", market)
+  resp = priv_req(req)
+  ret = list()
+  if (length(resp$result) > 0) {
+    for(i in 1:length(resp$result)) {
+      for (j in 1:length(resp$result[[i]])) {
+        if (is.null(resp$result[[i]][[j]])) resp$result[[i]][[j]] = NA
+      }
+    }
+    ret = Reduce(rbind, Map(as_data_frame, resp$result))
+    names(ret) = camel_to_snake(names(ret))
+    ret$time_stamp = strptime(ret$time_stamp, "%Y-%m-%d %H:%M:%S", tz="GMT")
+  }
+  resp$result = ret
+  resp
 }
 
